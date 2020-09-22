17b61dd3f286cddab9e31303dbed1dac1e3c1a6e
Biswa <biswapriyah@gmail.com>
2020-09-22 16:00:40 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{count}</div>
+     <button onClick = {() => setCount((prevCount) => prevCount + 1)}>Click me to add</button>
+     <button onClick = {() => setCount((prevCount) => prevCount - 1)}>Click me to substract</button>
     </div>
   );
 }
